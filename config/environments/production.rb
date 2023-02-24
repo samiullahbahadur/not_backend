4ab267c9f17ddd85201b2dc0b9dd5549f381c967
samiullah <bahadursamiullah@gmail.com>
2023-02-24 13:24:55 +0430

    fix issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  secret_key_base = "bin/rails credentials:edit"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
